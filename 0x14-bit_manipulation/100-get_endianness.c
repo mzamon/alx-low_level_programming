3a7c0575843f90e98c24ee24a4b93aaf0cb97e28
mzamon <ndlovu.richmond@gmail.com>
2023-10-27 09:39:03 -0700

    upload

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+int get_endianness(void)
+{
+	int a;
+	char *b;
+
+	a = 1;
+	b = (char *)&a;
+	return (*b);
+}
---
+++ 0x14-bit_manipulation/5-flip_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int a, countbit = 0;
+	unsigned long int current;
+	unsigned long int exclusive = n ^ m;
+
+	for (a = 63; a >= 0; a--)
+	{
+		current = exclusive >> a;
+		if (current & 1)
+			countbit++;
+	}
+
+	return (countbit);
+}
